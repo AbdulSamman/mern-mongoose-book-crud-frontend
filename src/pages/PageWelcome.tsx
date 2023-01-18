7efdbc93ee7e1f384395c4c1f5cd7acabbad4f73
Your Name <you@example.com>
2023-01-18 11:36:48 +0100

    data showing on welcomepages

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -3,14 +3,33 @@ import { AppContext } from '../AppContext';
 import { Helmet } from 'react-helmet';
 
 export const PageWelcome = () => {
-	const { appTitle } = useContext(AppContext);
+	const { appTitle, books } = useContext(AppContext);
 
 	return (
 		<div className="pageWelcome">
 			<Helmet>
 				<title>{appTitle} - Welcome</title>
 			</Helmet>
-			<p>Welcome to this site</p>
+			<p>There are {books.length} books:</p>
+			<div className="books">
+				{books.map((book) => {
+					return (
+						<div className="book" key={book._id}>
+							<img src={book.imageUrl} />
+							<div className="info">
+								<div className="title">
+									{' '}
+									<a href={book.buyUrl}></a> {book.title}
+								</div>
+								<div className="description">
+									{book.description}
+								</div>
+								<div className="language">{book.language}</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
